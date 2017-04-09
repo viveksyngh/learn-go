fa1f8b73a9b0e1167d0680c3f3e06a943eed8c08
Vivek Singh <vivek@happay.in>
2017-04-10 02:21:31 +0530

    added collection functions

---
+++ collection-functions.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var strs = []string{"peach", "apple", "pear", "plum"}
+	fmt.Println(Index(strs, "pear"))
+	fmt.Println(Include(strs, "grape"))
+	fmt.Println(All(strs, func(v string) bool {
+		return strings.HasPrefix(v, "p")
+	}))
+
+	fmt.Println(Filter(strs, func(v string) bool {
+		return strings.Contains(v, "e")
+	}))
+
+	fmt.Println(Map(strs, strings.ToUpper))
+}
+
+func Index(vs []string, p string) int {
+	for i, v := range vs {
+		if v == p {
+			return i
+		}
+	}
+	return -1
+}
+
+func Include(vs []string, p string) bool {
+	return Index(vs, p) >= 0
+}
+
+func Any(vs []string, f func(string) bool) bool {
+	for _, v := range vs {
+		if f(v) {
+			return true
+		}
+	}
+	return false
+}
+
+func All(vs []string, f func(string) bool) bool {
+	for _, v := range vs {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+func Filter(vs []string, f func(string) bool) []string {
+	vsf := make([]string, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
+}
+
+func Map(vs []string, f func(string) string) []string {
+	vsm := make([]string, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
